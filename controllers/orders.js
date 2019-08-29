7a465e8d5939b9943e8db6c685d0e4879a939a2a
Samantha <sami.shackelford@gmail.com>
2019-08-29 11:38:28 -0700

    progress on show functionality

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,13 +1,11 @@
 var Dish = require('../models/dish');
 var Order = require('../models/order');
-
-
+var Customer = require('../models/customer');
 
 module.exports = {
     addDish,
-    // showOrder
+    showOrder
 }
-
 
 function addDish(req, res) {
     //find active order for the logged in user
@@ -15,24 +13,24 @@ function addDish(req, res) {
         customer: req.user._id, 
         paid: false
     }, function (err, activeOrder) {
+        console.log('////////////////', req.params.id);
         if (!activeOrder) activeOrder = new Order({customer: req.user._id});
         activeOrder.lineItems.push({ dish: req.params.id });
+        console.log('AFTER PUSHHH', req.params.id);
+
         console.log('ACTIVE ORDER\'S LINE ITEMS', activeOrder.lineItems);
         // save the activeOrder containing the lineItem (dish)
         activeOrder.save(function (err) {
             res.redirect('/lunch');
         });
     });
-    showOrder();
 }
 
-
 function showOrder(req, res){
-    console.log('SHOW ORDER FUNCTION CALLED', );
-    Order.findOne({
-        customer: req.user._id, 
-        paid:false
-    }, function(err, order){
-        console.log(order);
+    console.log('SHOW ORDER FUNCTION CALLED');
+
+    Order.lineItems.findById(req.params.id, function(err, order){
+        console.log('showOrder order', order);
+        res.render('/order');
     })
 }
--- routes/orders.js
+++ routes/orders.js
@@ -3,10 +3,14 @@ const router = express.Router();
 var ordersCtrl = require('../controllers/orders');
 
 router.post('/add-dish/:id', isLoggedIn, ordersCtrl.addDish);
+router.get('/orders/:id', isLoggedIn, ordersCtrl.showOrder);
+
 
 function isLoggedIn(req, res, next){
     if (req.isAuthenticated()) return next();
     res.redirect('/auth/google');
 }
+
+
 
 module.exports = router;
